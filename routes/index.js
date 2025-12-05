77749d520c141a1cbc69bbf4e0d0c77c60accd28
Fuyu Y Por <fuyupor@u.boisestate.edu>
2025-12-05 08:05:34 +0000

    Test Commenting System

--- routes/index.js
+++ routes/index.js
@@ -1,30 +1,7 @@
 var express = require('express');
 var router = express.Router();
-/* GET home page. */
 
-router.get('/', function(req, res, next){
-  try {
-    req.db.query('SELECT * FROM todos;', (err, results) => {
-      if (err) {
-        console.error('Error fetching todos:', err);
-        return res.status(500).send('Error fetching todos');
-      }
-      res.render('index', {title: "Downtown Donuts"});
-    });
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
-});
-
-router.get('/index', (req, res) => {
-  try {
-      res.render('index', {title: "Downtown Donuts"});
-  } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
-  }
-});
+/* Page Navigation*/
 
 router.get('/about', (req, res) => {
   try {
@@ -46,17 +23,42 @@ router.get('/menu', (req, res) => {
 
 router.get('/reviews', (req, res) => {
     try {
-      res.render('reviews', {title: "Downtown Donuts"});
+    req.db.query('SELECT * FROM comments;', (err, results) => {
+      if (err) {
+        console.error('Error fetching comments:', err);
+        return res.status(500).send('Error fetching comments');
+      }
+      res.render('reviews', { title: 'Downtown Donuts', comments: results });
+    });
+  } catch (error) {
+    console.error('Error fetching comments:', error);
+    res.status(500).send('Error fetching comments');
+  }
+});
+
+/* GET home page. */
+
+// Renders Inital
+router.get('/', function(req, res, next){
+  try {
+    req.db.query('SELECT * FROM todos;', (err, results) => {
+      if (err) {
+        console.error('Error fetching todos:', err);
+        return res.status(500).send('Error fetching todos');
+      }
+      res.render('index', { title: 'My Simple TODO', todos: results });
+    });
   } catch (error) {
     console.error('Error fetching items:', error);
     res.status(500).send('Error fetching items');
   }
 });
 
+// Creates Task
 router.post('/create', function (req, res, next) {
-    const { comment } = req.body;
+    const { task } = req.body;
     try {
-      req.db.query('INSERT INTO comments (comment) VALUES (?);', [comment], (err, results) => {
+      req.db.query('INSERT INTO todos (task) VALUES (?);', [task], (err, results) => {
         if (err) {
           console.error('Error adding todo:', err);
           return res.status(500).send('Error adding todo');
@@ -70,7 +72,8 @@ router.post('/create', function (req, res, next) {
       res.status(500).send('Error adding todo');
     }
 });
-/*
+
+// Deletes Task
 router.post('/delete', function (req, res, next) {
     const { id } = req.body;
     try {
@@ -88,5 +91,73 @@ router.post('/delete', function (req, res, next) {
         res.status(500).send('Error deleting todo:');
     }
 });
-*/
+
+// Edits a task
+router.post('/edit', function (req, res, next) {
+    const { id } = req.body;
+    const { editor } = req.body;
+    try {
+      // edit task description 
+      req.db.query('UPDATE todos SET task = ? WHERE id = ?;', [editor, id], (err, results) => {
+
+        if (err) {
+          console.error('Error editing todo:', err);
+          return res.status(500).send('Error adding todo');
+        }
+        console.log('Todo edited successfully:', results);
+        // Redirect to the home page after adding
+        res.redirect('/');
+      });
+
+    } catch (error) {
+      console.error('Error editing todo:', error);
+      res.status(500).send('Error editing todo');
+      
+    }
+});
+
+// Switches the value of "complete" when the "complete" button is pressed
+router.post('/complete', function (req, res, next) {
+    const { id } = req.body;
+    try {
+      // edit task description 
+      req.db.query('UPDATE todos SET completed = NOT completed WHERE id = ?;', [id], (err, results) => {
+
+        if (err) {
+          console.error('Error completing task:', err);
+          return res.status(500).send('Error completing task');
+        }
+        console.log('Todo completed successfully:', results);
+        // Redirect to the home page after adding
+        res.redirect('/');
+      });
+
+    } catch (error) {
+      console.error('Error completing task:', error);
+      res.status(500).send('Error completing task');
+      
+    }
+});
+
+
+/* Leave a Comment */
+router.post('/comment', function (req, res, next) {
+  const { name } = req.body;
+  const { comment } = req.body;
+    try {
+      req.db.query('INSERT INTO comments (commenterName, comment) VALUES (?);', [name, comment], (err, results) => {
+        if (err) {
+          console.error('Error adding comment:', err);
+          return res.status(500).send('Error adding comment');
+        }
+        console.log('Comment added successfully:', results);
+        // Redirect to the home page after adding
+        res.redirect('/reviews');
+      });
+    } catch (error) {
+      console.error('Error adding comment:', error);
+      res.status(500).send('Error adding comment');
+    }
+});
+
 module.exports = router;
