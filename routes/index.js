c584cb427a5c961c273c35d302c48cd9496f4275
Fuyu Y Por <fuyupor@u.boisestate.edu>
2025-12-12 01:20:44 +0000

    Added Comments

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,27 @@ var router = express.Router();
 
 /* Page Navigation*/
 
+// Renders About Page
 router.get('/about', (req, res) => {
   try {
       res.render('about', {title: "Downtown Donuts"});
   } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+    console.error('Error Rendering Page:', error);
+    res.status(500).send('Error Rendering Page');
   }
 });
 
+// Renders Menu Page
 router.get('/menu', (req, res) => {
     try {
       res.render('menu', {title: "Downtown Donuts"});
   } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+    console.error('Error Rendering Page:', error);
+    res.status(500).send('Error Rendering Page');
   }
 });
 
+// Renders Review Page and results
 router.get('/reviews', (req, res) => {
     try {
     req.db.query('SELECT * FROM comments;', (err, results) => {
@@ -36,20 +39,19 @@ router.get('/reviews', (req, res) => {
   }
 });
 
-/* GET home page. */
 
-// Renders Inital
+// Renders Inital Page
 router.get('/', function(req, res, next){
   try {
       res.render('index', {title: "Downtown Donuts"});
   } catch (error) {
-    console.error('Error fetching items:', error);
-    res.status(500).send('Error fetching items');
+    console.error('Error Rendering Page:', error);
+    res.status(500).send('Error Rendering Page');
   }
 });
 
 
-/* Leave a Comment */
+// Leaves a comment based on form
 router.post('/comment', function (req, res, next) {
   const { name } = req.body;
   const { comment } = req.body;
